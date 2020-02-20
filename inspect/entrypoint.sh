d38df0955676fe529461feeb0d0a73a8177ee3ef
Rhys Elsmore <me@rhys.io>
2020-02-20 10:56:44 -0800

    Adds inspect command

---
+++ inspect/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+TEMPLATE_FILE=$1
+if [[ ! -f "$TEMPLATE_FILE" ]]; then
+    echo "::error ::File ${TEMPLATE_FILE} could not be found. Please check your paths"
+    exit 1
+fi
+
+set +e
+# Run packer template validator
+OUTPUT=$(sh -c "packer inspect ${TEMPLATE_FILE}" 2>&1)
+CODE=$?
+
+if [ $CODE -ne 0 ]; then
+    echo "::error ::Template Inspection Failed"
+    echo "$OUTPUT"
+    exit 1
+fi
+
+echo "$OUTPUT"
