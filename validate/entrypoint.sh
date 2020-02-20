4f1035fb10db08cfbe792e80b834861f8fc6b711
Rhys Elsmore <me@rhys.io>
2020-02-20 10:51:47 -0800

    Adds validation

--- validate/entrypoint.sh
+++ validate/entrypoint.sh
@@ -6,3 +6,16 @@ if [[ ! -f "$TEMPLATE_FILE" ]]; then
     echo "::error ::File ${TEMPLATE_FILE} could not be found. Please check your paths"
     exit 1
 fi
+
+set +e
+# Run packer template validator
+OUTPUT=$(sh -c "packer validate ${TEMPLATE_FILE}" 2>&1)
+CODE=$?
+
+if [ $CODE -ne 0 ]; then
+    echo "::error ::Template Validation Failed"
+    echo "$OUTPUT"
+    exit 1
+fi
+
+echo "Template Successfully Validated"
